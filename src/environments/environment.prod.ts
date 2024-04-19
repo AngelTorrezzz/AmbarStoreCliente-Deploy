ad4e5ce144934ce09e13f1b16bf40c793b3169d8
Angel Alvarado <aata030115@gs.utm.mx>
2024-04-19 00:49:59 -0600

    cambiando rutas a los servidores de de render2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //API_URI: 'http://3.215.214.85/api',
   //API_URI_CORREOS: "http://3.215.214.85:8080",
-  API_URI: 'http://localhost:3000/api',
-  API_URI_CORREOS: "http://localhost:3001",
-  API_URI_IMAGENES: "http://localhost:3002"
+  API_URI: 'https://ambar-index-vsrq.onrender.com/api',
+  API_URI_CORREOS: "https://ambar-app.onrender.com",
+  API_URI_IMAGENES: "https://ambar-imagen.onrender.com"
 };
